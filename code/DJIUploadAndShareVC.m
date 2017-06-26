4698e83bc6299ae7d4a3fb14034c114f6ad0756f
spy <spy.liu@dji.com>
2017-06-26 20:16:46 +0800

    Merge branch 'code/b2' into code/b1
    # Conflicts:
    #	code/DJIUploadAndShareVC.h
    #	code/DJIUploadAndShareVC.m

--- code/DJIUploadAndShareVC.m
+++ code/DJIUploadAndShareVC.m
@@ -268,7 +268,7 @@ UIViewControllerTransitioningDelegate, UIDocumentInteractionControllerDelegate> 
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    [super viewDidLoad1];
     //    self.title = NSLocalizedString(@"share", nil);
     UIView* leftView;
     if (self.videoProject || (!self.uploadFile.isPhoto && self.fromMakeMovieVC == YES)) {
